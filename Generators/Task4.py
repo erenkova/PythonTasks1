5a05c4b6434af8d6a17d27326694cfc194b4a6d9
erenkova <arinaerenkova@gmail.com>
2023-06-28 15:21:46 +0300

    task 4 generators

--- Generators/Task4.py
+++ Generators/Task4.py
@@ -2,3 +2,12 @@
 Напишите генераторное выражения. Для генерации словаря где ключем выступают числа от 0 до 10. А значения эти же числа в 16чной системе счисления.
 Работу с 16чной системой найдите в документации Python
 """
+generator = {i:hex(i) for i in range(11)}
+for i,j in generator.items():
+    print(i,j)
+
+generator = {i:hex(i) for i in range(11)}
+print(generator)
+
+generator = {i:hex(i) for i in range(11)}
+print(*generator)
